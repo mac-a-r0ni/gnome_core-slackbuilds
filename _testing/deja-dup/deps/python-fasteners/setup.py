a02459833c18296cfae20fe286f122af1d4bf874
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2022-12-01 02:45:30 -0500

    cleanup & rearrange

---
+++ _testing/deja-dup/deps/python-fasteners/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='fasteners',
+    version='0.17.3',
+    author='Joshua Harlow',
+    author_email='',
+    maintainer='Paulius Sarka',
+    maintainer_email='',
+    packages=['fasteners'],
+    url='https://github.com/harlowja/fasteners',
+    license='LICENSE',
+    description='Cross-platform locks for thread and processes',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
