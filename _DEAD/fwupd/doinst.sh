bb6f34727ee9ea9200f950e9933ed35e53e4670d
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2024-10-15 15:47:34 -0400

    sync the mess I've created

---
+++ _DEAD/fwupd/doinst.sh
@@ -0,0 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config etc/fwupd/fwupd.conf.new
+config etc/fwupd/remotes.d/lvfs-testing.conf.new
+config etc/fwupd/remotes.d/lvfs.conf.new
+config etc/fwupd/remotes.d/vendor-directory.conf.new
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
